ada1e491f3cbd351349b0171523b96d1e5f65fce
mattdvhope <mattagape@me.com>
2020-04-17 00:16:26 +0700

    Tweaked the href

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,29 @@ import { Link } from "gatsby"
 
 export default () => (
   <div>
-    <p>Hello world!</p>
+    <p>In Join CEP!!</p>
     <Link to="/account/">Go to your account</Link>
+
+    <h2>
+      <a
+        href={`https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1654045933&redirect_uri=${process.env.GATSBY_API_URL}&state=${makeState(10)}&scope=profile%20openid&max_age=360000&ui_locales=th&bot_prompt=aggressive`}
+        style={{
+          color: `rgb(45, 49, 121)`,
+          fontFamily: `Athiti`,
+        }}
+      >
+        Click here to enter via LINE
+      </a>
+    </h2>
   </div>
 )
+
+function makeState(length) {
+   let result           = '';
+   let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   let charactersLength = characters.length;
+   for ( let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
