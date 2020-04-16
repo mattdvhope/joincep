0db8b82a3b96329e332f71204fe32f60e7567800
mattdvhope <mattagape@me.com>
2020-04-17 00:18:01 +0700

    Commented out console logs

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -14,17 +14,6 @@ export const isAuthenticated = () => {
   return tokens.idToken !== false
 }
 
-
-// LINE info
-// {
-//   "sub": "line|U1322cdfc6d9326c022e807508314c569",
-//   "nickname": "Matt",
-//   "name": "Matt",
-//   "picture": "https://profile.line-scdn.net/0hFu18iD4fGVoMQTJ5Dm1mDTAEFzd7bx8SdHNWOS0UFWl2JA5ZMndfOyARQj4lcVlfOCMEOS4STm4l",
-//   "updated_at": "2020-04-11T13:25:33.876Z"
-// }
-
-
 const auth = isBrowser
   ? new auth0.WebAuth({
       domain: process.env.GATSBY_AUTH0_DOMAIN,
@@ -73,20 +62,20 @@ const setSession = (cb = () => {}) => (err, authResult) => {
 }
 
 export const checkSession = callback => {
-  console.log("in checkSession");
+  // console.log("in checkSession");
   const isLoggedIn = window.localStorage.getItem("isLoggedIn")
 
   if (isLoggedIn === "false" || isLoggedIn === null) {
-    protectedRoutes = [`/account`, `/callback`];
-    callback()
+    protectedRoutes = [`/account`];
+    callback();
   } else {
-    protectedRoutes = [`/account`, `/callback`, `/`];
+    protectedRoutes = [`/account`, `/`];
   }
-  console.log("current protectedRoutes: ", protectedRoutes);
+  // console.log("current protectedRoutes: ", protectedRoutes);
   const isProtectedRoute = protectedRoutes
     .map(route => window.location.pathname.includes(route))
     .some(route => route)
-  console.log("isProtectedRoute: ", isProtectedRoute);
+  // console.log("isProtectedRoute: ", isProtectedRoute);
   if (isProtectedRoute) {
     auth.checkSession({}, setSession(callback))
   }
